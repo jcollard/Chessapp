a89c1f893ec9b378cfbe88b9c996e773d1d8037c
Joseph Collard <jcollard@users.noreply.github.com>
2022-11-20 08:54:30 -0700

    refactor: Simplify RookPiece Logic.

--- ConsoleApp9/Piece/RookPiece.cs
+++ ConsoleApp9/Piece/RookPiece.cs
@@ -4,65 +4,42 @@ public class RookPiece : AbstractPiece
 {
     public RookPiece(string symbol, PieceColor color, (int, int) position, GameState gameState) : base(symbol, color, position, gameState) { }
 
+    private static int GetIncrement(int start, int target)
+    {
+        if (start == target)
+        {
+            return 0;
+        }
+        else if (start > target)
+        {
+            return -1;
+        }
+        else
+        {
+            return 1;
+        }
+    }
+
     protected override bool SubLogic((int row, int col) start, (int row, int col) target)
     {
-        string targetSymbol = Program.changes.BoardLayout[target.row, target.col];
-
-        int diff = Math.Abs(start.row - target.row) - Math.Abs(start.col - target.col);
-        if (Math.Abs(diff) != Math.Abs(start.row - target.row) && Math.Abs(diff) != Math.Abs(start.col - target.col))
+        int rowInc = GetIncrement(start.row, target.row);
+        int colInc = GetIncrement(start.col, target.col);
+        if (Math.Abs(rowInc) + Math.Abs(colInc) != 1)
         {
             return false;
         }
-        if (0 != Math.Abs(start.row - target.row) && 0 != Math.Abs(start.col - target.col))
-        {
-            return false;
-        }
-        if (Math.Abs(start.row - target.row) != 0)
-        {
 
-            for (int i = start.row, j = start.col; i > 0 || i < 7;)
+        int row = start.row + rowInc;
+        int col = start.col + colInc;
+
+        while (row != target.row || col != target.col)
+        {
+            if (!this._gameState.IsEmpty((row, col)))
             {
-                if (i == target.row)
-                {
-                    break;
-                }
-                if (start.row > target.row)
-                {
-                    i--;
-                }
-                else
-                {
-                    i++;
-                }
-                if (Program.changes.pieces.Contains(Program.changes.BoardLayout[i, j]) && Program.changes.BoardLayout[i, target.col] != targetSymbol)
-                {
-                    return false;
-                }
+                return false;
             }
-        }
-        if (Math.Abs(start.col - target.col) != 0)
-        {
-            for (int i = start.row, j = start.col; j > 0 || j < 7;)
-            {
-                if (j == target.col)
-                {
-                    break;
-                }
-
-                if (start.col > target.col)
-                {
-                    j--;
-                }
-                else
-                {
-                    j++;
-                }
-
-                if (Program.changes.pieces.Contains(Program.changes.BoardLayout[i, j]) && Program.changes.BoardLayout[target.row, j] != targetSymbol)
-                {
-                    return false;
-                }
-            }
+            row += rowInc;
+            col += colInc;
         }
         return true;
     }
