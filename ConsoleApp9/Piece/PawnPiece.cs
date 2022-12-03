698c94abb568b00aeb1a4fc0170b7fc2168ec2e4
CodeItQuick <evanontario@hotmail.com>
2022-12-03 10:07:14 -0400

    remove inheritance from Pawn Piece

--- ConsoleApp9/Piece/PawnPiece.cs
+++ ConsoleApp9/Piece/PawnPiece.cs
@@ -1,10 +1,23 @@
 namespace Chess;
-public class PawnPiece : AbstractPiece
+public class PawnPiece : IPiece
 {
 
-    public PawnPiece(string symbol, PieceColor color, (int, int) position, GameState gameState) : base(symbol, color, position, gameState) { }
+    public bool IsCaptured { get; set; } = false;
+    public bool HasMoved { get; private set; }
+    public string Symbol { get; private set; }
+    public PieceColor Color { get; private set; }
+    public (int row, int col) Position { get; private set; }
+    protected readonly GameState _gameState;
+    public PawnPiece(string symbol, PieceColor color, (int, int) position, GameState gameState)
+    {
+        this.Symbol = symbol;
+        this.Color = color;
+        this.Position = position;
+        this._gameState = gameState;
+        this._gameState.SetPiece(position, this);
+    }
 
-    protected override bool SubLogic((int row, int col) target)
+    protected bool SubLogic((int row, int col) target)
     {
         int rowInc = this.Color == PieceColor.Blue ? 1 : -1;
         int targetRow = this.Position.row + rowInc;
@@ -37,4 +50,59 @@ public class PawnPiece : AbstractPiece
 
         return false;
     }
+    /// <inheritdoc/>
+    public bool Move((int row, int col) target)
+    {
+        if (this.Logic(target))
+        {
+            IPiece? other = this._gameState.GetPiece(target);
+            if (other != null)
+            {
+                other.IsCaptured = true;
+            }
+            this._gameState.ClearPiece(this.Position);
+            this._gameState.SetPiece(target, this);
+            this.Position = target;
+            HasMoved = true;
+            this._gameState.AddMove(this, target);
+            return true;
+        }
+        return false;
+    }
+
+    /// <inheritdoc/>
+    public List<(int, int)> GetMoves()
+    {
+        List<(int, int)> moves = new ();
+        for (int row = 0; row < 8; row++)
+        {
+            for (int col = 0; col < 8; col++)
+            {
+                if (this.Logic((row, col)))
+                {
+                    moves.Add((row, col));
+                }
+            }
+        }
+        return moves;
+    }
+
+    /// <inheritdoc/>
+    public bool Logic((int row, int col) target)
+    {
+        // Pieces cannot move onto themselves
+        if (this.Position == target)
+        {
+            return false;
+        }
+        // Cannot capture pieces of the same color
+        if(!this._gameState.IsEmpty(target) && !this.IsEnemyPiece(this._gameState.GetPiece(target)!))
+        {
+            return false;
+        }
+        return SubLogic(target);
+    }
+    
+    /// <inheritdoc/>
+    private bool IsEnemyPiece(IPiece other) => other.Color != this.Color;
 }
